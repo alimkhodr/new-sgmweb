a17a2e11f38dc191f27858d2c398f3c061bb1a69
alimoahmed <ali.m.khodr@gmail.com>
2024-09-03 14:10:38 -0300

    feat: check registro

---
+++ backend/src/api/User/checkReg.js
@@ -0,0 +1,28 @@
+const { poolPromise } = require('../../config/dbConfig');
+
+const checkUser = async (req, res) => {
+    const username = req.body.username;
+    try {
+        if (!username) {
+            return res.status(400).send('O nome de usuário deve ser fornecido.');
+        }
+        const pool = await poolPromise;
+        const result = await pool.request()
+            .input('username', username)
+            .query(`SELECT FUN_REGISTRO FROM FUNCIONARIO WHERE FUN_REGISTRO = @username AND FUN_STATUS = 'ATIVO'`);
+        
+        if (result.recordset.length === 0) {
+            return res.status(204).send('Usuário não encontrado.');
+        }
+        else{
+            return res.status(200).send('Usuário encontrado.');
+        }
+    } catch (error) {
+        console.error('Erro no servidor:', error);
+        res.status(500).send('Erro no servidor');
+    }
+};
+
+module.exports = {
+    checkUser
+};
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,16 +3,19 @@ const { login } = require('../controllers/authController');
 const { list_form_scrap } = require('../api/Scrap/listScrap');
 const { delete_scrap } = require('../api/Scrap/deleteScrap');
 const { user_nome } = require('../api/User/nameUser');
+const { checkUser } = require('../api/User/checkReg');
 const { list_linha } = require('../api/Linha/listLinha');
 const { createNewForm } = require('../api/Scrap/newForm');
+const { ace_telas } = require('../controllers/authMenu');
 const authenticateToken = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/list_form_scrap', list_form_scrap);
 router.post('/user_nome', user_nome);
+router.post('/checkUser', checkUser);
 router.post('/createNewForm', createNewForm);
-
+router.post('/ace_telas', ace_telas);
 router.post('/list_linha', list_linha);
 
 router.delete('/delete_scrap/:id', delete_scrap);
