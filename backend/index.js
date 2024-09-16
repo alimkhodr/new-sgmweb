65b95a5127fd3a9ff848f71d48f7c0d2d2dae847
alimoahmed <ali.m.khodr@gmail.com>
2024-09-16 08:53:41 -0300

    fix: nide.exe in web config

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./src/routes/authRoutes');
 
 const app = express();
-const port = process.env.PORT || 433;
+const port = 433;
 
 // const allowedOrigins = [
 //   'http://10.251.42.250:5173',
