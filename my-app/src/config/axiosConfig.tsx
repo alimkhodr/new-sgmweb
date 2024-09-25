0132d64bec9f0abb8f0dc9b769865537ed848e67
alimoahmed <ali.m.khodr@gmail.com>
2024-09-25 11:22:13 -0300

    feat: docker file

--- my-app/src/config/axiosConfig.tsx
+++ my-app/src/config/axiosConfig.tsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const port = 3000;
 const api = axios.create({
-  // baseURL: `https://10.251.42.93:${port}/api`,
-  baseURL: `https://10.251.42.158:${port}/api`,
+  baseURL: `https://10.251.42.93:${port}/api`,
+  // baseURL: `https://10.251.42.158:${port}/api`,
 });
 
 export default api;
