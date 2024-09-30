2e23fd04cf5217d2815617d87958279c54803648
alimoahmed <ali.m.khodr@gmail.com>
2024-09-30 12:14:42 -0300

    fix: docker

--- my-app/src/config/axiosConfig.tsx
+++ my-app/src/config/axiosConfig.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const port = 3000;
 const api = axios.create({
-  baseURL: `https://10.251.42.93:${port}/api`,
+  baseURL: `https://mfgsvr3:${port}/api`,
   // baseURL: `https://10.251.42.158:${port}/api`,
 });
 
